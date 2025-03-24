97af151a40a2b5766301bbc012c35159e155e8fb
PMagoga <pmagoga@gmail.com>
2025-03-23 22:41:10 -0300

    adicionando os verbos http

---
+++ src/main/java/com/magoga/math/SimpleMath.java
@@ -0,0 +1,35 @@
+package com.magoga.math;
+
+import com.magoga.exception.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SimpleMath {
+
+    public Double sum(Double numberOne, Double numberTwo)
+    {
+        return numberOne + numberTwo;
+    }
+    public Double subtraction(Double numberOne, Double numberTwo)
+    {
+        return numberOne - numberTwo;
+    }
+    public Double multiplication(Double numberOne, Double numberTwo)
+    {
+        return numberOne * numberTwo;
+    }
+    public Double divison(Double numberOne, Double numberTwo)
+    {
+        return numberOne / numberTwo;
+    }
+    public Double mean(Double numberOne, Double numberTwo)
+    {
+        return (numberOne + numberTwo)/2;
+    }
+
+    public Double squareroot(Double number)
+    {
+        return Math.sqrt(number);
+    }
+
+}
